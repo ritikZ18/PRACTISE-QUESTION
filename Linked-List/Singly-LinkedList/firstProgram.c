d2641f04b51a3df55a3ad1bb73ff67f30ed221db
ritikz18 <rzambre@asu.edu>
2025-01-31 13:57:16 -0700

    DNE

---
+++ Linked-List/Singly-LinkedList/firstProgram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Learn things, implement data type, memory allocation , pointers
+int main(){
+    float num = 12 * 0.4111;
+    int x = 31,y =1231, z= 23 ;
+    float operation = (float) y / ((float) x/ z) ;
+    printf("Hello World \n This is First C Program of Ritik, sum is : %f \n operation cost is : $%f", num, operation*100);
+    return 0;
+}
+
+
