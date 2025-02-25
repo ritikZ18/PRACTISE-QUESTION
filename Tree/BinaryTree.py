991c1cd9f57c69522ecf8ba4ce26849460f97909
ritikz18 <rzambre@asu.edu>
2025-02-24 21:07:04 -0700

    min , max,  delete, size

--- Tree/BinaryTree.py
+++ Tree/BinaryTree.py
@@ -19,6 +19,7 @@ class Node:
 class BST: 
     def __inti__(self):
         self.root = None
+        self.item_count = 0
         
         # rinsert does the operation, insert just makes tree
     def insert(self, data):
@@ -41,7 +42,7 @@ class BST:
     def search(self,data):
         self.root = self.rsearch(self.root, data)
     
-    def rsearch(self, root, data):
+    def rsearch(self, root, data):      #recursive function to atraverse and search, delete, getData, etc 
         if (root is None )or (root.item == data) : 
             return root
         
@@ -84,8 +85,56 @@ class BST:
     '''
     1. find min value item node
     2. find max value item node
-    3. delete method to delete node 
+    3. delete method to delete node  --> we will replace the parent with predecesor/succesor
     4. size method to return tree size
     '''
+    
+    def minValue(self, temp ):  #temp pointing the root node 
+        current = temp 
+        while current.left is not None : 
+            current = current.left
+        return current.item
+    
+    def maxValue(self,temp) : 
+        current = temp
+        while current.right is not None : 
+            current = current.right
+        return current.item
+    
+    def delete(self, data):
+        self.root = rdelete(self.root, data)
+        
+    def rdelete(self,root, data):
+        if root is None : 
+            return root
+        
+        if data < root.item : 
+            root.left = self.rdelete(root.left, data) 
+        
+        elif data > root.item : 
+            root.right = self.rdelete(root.right, data) 
+        
+        else : 
+            if root.left is None :      #single child
+                return root.right
+            
+            elif root.right is None :  #single child
+                return root.left
+            
+            self.root = self.minValue(root.right)  #successor
+            #self.root = self.maxValue(root.left)    # predecesor
+            
+            self.rdelete(root.right, root.item )   
+        
+        return root
+        
+        
+        def size(self):
+             return len(self.inorder())
+        
+        
+
+         
+    
     
     
